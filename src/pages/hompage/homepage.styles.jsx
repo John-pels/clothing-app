de8614d74ebcb26fd22a271998637574de38ed23
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-12-15 07:21:52 +0100

    I Write JS in CSS with Styled-components for this component

---
+++ src/pages/hompage/homepage.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HomePageContainer = styled.div`
+         display: flex;
+         flex-direction: column;
+         flex-wrap: wrap;
+         align-items: center;
+         padding: 20px 10px;
+       `;
