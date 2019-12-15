df43bf2ace032448e7802969959fba727fb8ef2b
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-12-15 08:21:24 +0100

    I import the styled-component format into the component

--- src/pages/hompage/homepage.component.jsx
+++ src/pages/hompage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './homepage.styles.scss';
+import {HomePageContainer} from './homepage.styles';
 import Directory from '../../components/directory/directory.component';
 
 
 const Homepage = () => (
-    <div className="homepage" >
+    <HomePageContainer>
         <Directory />
-    </div >
+    </HomePageContainer>
 );
 
 export default Homepage;
