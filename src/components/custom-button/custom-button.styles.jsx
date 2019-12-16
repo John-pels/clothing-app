ea31e57a0940ed63f7fec6532cf4bc8938ffec0b
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-12-16 15:21:36 +0100

    Custom Button
    I create custom-button styled component

---
+++ src/components/custom-button/custom-button.styles.jsx
@@ -0,0 +1,64 @@
+import styled, { css } from "styled-components";
+
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+    transition: background 0.8s ease-in-out;
+  }
+`;
+
+export const invertedButtonStyles = css`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  transition: background 0.8s ease-in-out;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+  }
+`;
+
+export const googleSignInStyles = css`
+  background-color: #4285f4;
+  color: white;
+
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+    transition: background 0.8s ease-in-out;
+  }
+`;
+
+const getButtonStyles = props => {
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
+
+export const CustomButtonContainer = styled.button`
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 0 35px 0 35px;
+  font-size: 15px;
+  color: white;
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: bolder;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+
+  ${getButtonStyles}
+`;
