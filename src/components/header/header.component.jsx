5aad1394ec28f34d11ac14213fae6da98c55624c
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2019-12-16 15:23:30 +0100

    Header component
    I import header styled-component into the component itself

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,6 @@ import {
   HeaderContainer,
   LogoContainer,
   OptionsContainer,
-  OptionDiv,
   OptionLink
 } from "./header.styles";
 
@@ -24,7 +23,9 @@ const Header = ({ currentUser, hidden }) => (
       <OptionLink to="/shop">SHOP</OptionLink>
       <OptionLink to="/shop">CONTACT</OptionLink>
       {currentUser ? (
-        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </OptionLink>
       ) : (
         <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
