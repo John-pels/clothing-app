60e1c906d5d2865fb2a77650766c0d26b1c5971a
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-02-14 23:37:35 +0100

    I replace redux-thunk with redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,20 @@
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+
+import createSagaMiddleware from "redux-saga";
+import { fetchCollectionsStart } from "./shop/shop.sagas";
 
 import rootReducer from "./root-reducer";
+const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [thunk];
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+sagaMiddleware.run(fetchCollectionsStart);
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+export default { store, persistStore };
