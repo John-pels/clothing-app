207405395faa464dd0cbc3f3c54a045b4b73dea4
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-02-16 07:32:25 +0100

    I implement the single sign in success and failure to the redux saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,21 +1,37 @@
 import { takeLatest, put, all, call } from "redux-saga/effects";
 import userActionTypes from "./user.types";
-import { googleSignInSuccess, googleSignInFailure } from "./user.actions";
+import { signInSuccess, signInFailure } from "./user.actions";
 import {
   auth,
   googleProvider,
   createUserProfileDocument
 } from "../../firebase/firebase.utils";
+
+export function* getSnapshotFromUserAuth(userAuth) {
+  try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userSnapshot = yield userRef.get();
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
+
 export function* signInWithGoogle() {
   try {
     const { user } = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(
-      googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-    );
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
+  }
+}
+
+export function* signInWithEmail({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    yield getSnapshotFromUserAuth(user);
+  } catch (error) {
+    put(signInFailure(error));
   }
 }
 
@@ -23,6 +39,10 @@ export function* onGoogleSignInStart() {
   yield takeLatest(userActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* onEmailSignInStart() {
+  yield takeLatest(userActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
+}
+
 export function* userSagas() {
-  yield all([call(onGoogleSignInStart)]);
+  yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
 }
