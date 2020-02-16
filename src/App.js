8c8811f043a9867a189a620490651a7ad599c241
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-02-16 07:36:37 +0100

    I remove the mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-
-// import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { createStructuredSelector } from "reselect";
 import "./App.css";
@@ -17,9 +14,7 @@ import "./App.css";
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
-  componentDidMount() {
-    // const { setCurrentUser } = this.props;
-  }
+  componentDidMount() {}
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -54,8 +49,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
