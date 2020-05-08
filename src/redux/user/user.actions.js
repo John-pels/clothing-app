531f3913430f6040fe9182012e1f16dcab9342da
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-05-08 04:13:56 -0400

    I add sign-out actions to it

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,19 +1,36 @@
 import userActionTypes from "./user.types";
 
 export const googleSignInStart = () => ({
-  type: userActionTypes.GOOGLE_SIGN_IN_START
+  type: userActionTypes.GOOGLE_SIGN_IN_START,
 });
-export const signInSuccess = user => ({
+export const signInSuccess = (user) => ({
   type: userActionTypes.SIGN_IN_SUCCESS,
-  payload: user
+  payload: user,
 });
 
-export const signInFailure = error => ({
+export const signInFailure = (error) => ({
   type: userActionTypes.SIGN_IN_FAILURE,
-  payload: error
+  payload: error,
 });
 
-export const emailSignInStart = emailAndPassword => ({
+export const emailSignInStart = (emailAndPassword) => ({
   type: userActionTypes.EMAIL_SIGN_IN_START,
-  payload: emailAndPassword
+  payload: emailAndPassword,
+});
+
+export const checkUserSession = () => ({
+  type: userActionTypes.CHECK_USER_SESSION,
+});
+
+export const signOutStart = () => ({
+  type: userActionTypes.SIGN_OUT_START,
+});
+
+export const signOutSuccess = () => ({
+  type: userActionTypes.SIGN_OUT_SUCCESS,
+});
+
+export const signOutFailure = (error) => ({
+  type: userActionTypes.SIGN_OUT_Failure,
+  payload: error,
 });
