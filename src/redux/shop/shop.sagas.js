811222cda590f4d5ff790ac3cdd7f7778d3ddbc3
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-05-11 16:21:09 +0100

    I create a combined saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,13 +1,13 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 
 import {
   firestore,
-  convertCollectionsSnapshotToMap
+  convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
 
 import {
   fetchCollectionsSuccess,
-  fetchCollectionsFailure
+  fetchCollectionsFailure,
 } from "./shop.actions";
 
 import ShopActionTypes from "./shop.types";
@@ -31,4 +31,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
